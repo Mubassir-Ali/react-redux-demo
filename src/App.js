3ed697e4608e90b90b7f60735c990367af907b5f
Mubassir-Ali <mubassir11@gmail.com>
2021-03-26 20:07:45 +0500

    use useSelector & useDispatch hooks instead of connect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CakeContainer from './components/CakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer'
 import { Provider } from 'react-redux';
 import store from './redux/store'
 import './App.css';
@@ -7,7 +8,8 @@ function App() {
 	return (
 		<Provider store={store}>
 			<div className="App">
-				<CakeContainer />
+				{/* <CakeContainer /> */}
+        <HooksCakeContainer/>
 			</div>
 		</Provider>
 	);
